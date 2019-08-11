9c0357c34e94e06bade9736b7a3935498381b5bc
jenniferHarper <jenniferHarper@users.noreply.github.com>
2019-08-11 13:10:43 +0200

    before scroll left added

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 //         location.reload();
 // });
 
-// if (!Modernizr.touch) {
+// if (!Modernizr.touch) {}
 var controller = new ScrollMagic.Controller();
 TweenLite.defaultEase =  Power2.easeInOut;
   
@@ -18,7 +18,7 @@ $(".clip-me").each(function() {
 	tl.to(cov, 0, {background:"#92c4b5"})
 	tl.from(cov, 1, { scaleX: 0, transformOrigin: "right",ease:Power4.easeOut });
 	tl.to(cov, 1, { scaleX: 0, transformOrigin: "left" ,ease:Power4.easeOut}, "reveal");
-	tl.from(img, 1.5, { opacity: 0,ease:Power4.easeOut }, "reveal");
+	tl.from(img, 1, { opacity: 0,ease:Power4.easeOut }, "reveal");
   
 	var scene = new ScrollMagic.Scene({
 	  triggerElement: this,
@@ -33,11 +33,13 @@ $(".clip-me").each(function() {
 
 
 //   uptext
-
+if ($( window ).width() >= 768) {
   $("section").each(function() {
 	var tl = new TimelineMax();
-	var cov = $(this).find(".up-text");  
-	tl.staggerFrom(cov, 1, { y: '200%', ease:Power1.easeOut },0.15);  
+	var cov = $(this).find(".up-text"); 
+	tl.from(cov,.8, { opacity:0, ease:Power1.easeOut },'0');  
+	tl.from(cov, .8, { y: '200%', ease:Power1.easeOut },'0');  
+
 	var scene = new ScrollMagic.Scene({
 	  triggerElement: this,
 	  triggerHook: .7,
@@ -46,7 +48,27 @@ $(".clip-me").each(function() {
 	  .addIndicators()
 	  .addTo(controller);
   });
-// }
+}
+
+
+$(".full-img-feature").each(function() {
+	var tl = new TimelineMax();
+	var img = $(this).find(".img-fluid");
+	
+	tl.from(img, 3, { width:'200%', height:'200%', transformOrigin:"50% 50%", ease:Power4.easeOut }, "reveal");
+
+
+	var scene = new ScrollMagic.Scene({
+	  triggerElement: this,
+	  triggerHook: 'onEnter',
+	  offset:200
+
+	})
+	  .setTween(tl)
+	  .addIndicators()
+	  .addTo(controller);
+  });
+
 
 })(jQuery);
 
